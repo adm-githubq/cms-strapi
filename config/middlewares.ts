e8c141d0b46164da125defcf8ae0fba71857b44b
German Cabarcas <gcc@unironical.com>
2024-03-27 12:28:07 -0500

    Removes S3 asset hosting config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,35 +1,6 @@
 module.exports = ({ env }) => [
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            `https://${env("AWS_BUCKET")}.s3.${env(
-              "AWS_REGION"
-            )}.amazonaws.com/`,
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            `https://${env("AWS_BUCKET")}.s3.${env(
-              "AWS_REGION"
-            )}.amazonaws.com/`,
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
